c898f4cc7d584a1fc7318a1b39ccad57351299d3
Robera Geleta <robera.geleta@vensi.com>
2015-03-24 17:42:05 -0500

    Initial Commit

---
+++ ShakeSwipe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShakeSwipe
+//
+//  Created by ROBERA GELETA on 3/24/15.
+//  Copyright (c) 2015 ROBERA GELETA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShakeSwipe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShakeSwipe
+//
+//  Created by ROBERA GELETA on 3/24/15.
+//  Copyright (c) 2015 ROBERA GELETA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
